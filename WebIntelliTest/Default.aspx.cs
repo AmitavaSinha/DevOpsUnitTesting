31ab7155ac6f169c59e588048af3d0613e1576ab
Amitava Sinha <Amitava.Sinha@hcl.com>
2018-10-26 15:56:11 +0530

    Initial Commit

---
+++ WebIntelliTest/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebIntelliTest.BusinessCode;
+
+namespace WebIntelliTest
+{
+    public partial class _Default : Page
+    {
+
+        General objGeneral = new General();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSub_ServerClick(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(Val1.Value);
+            int b = Convert.ToInt32(Val2.Value);
+            int result = objGeneral.AddTwoPositiveNumber(a, b);
+            Response.Write(result);
+        }
+
+        
+    }
+}
